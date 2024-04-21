7cc630abe67a1e552198e8dedc284b1c966bb600
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-21 11:26:40 +0530

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,80 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import ReactLoading from "react-loading";
 import "./App.css";
 import Card from "./Card";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(2);
+
+  const fetchData = useCallback(async () => {
+    if (loading)
+      return (
+        <div className="h-full flex justify-center items-center bg-blue-950 text-white">
+          <ReactLoading type="bars" color="white" height={100} width={150} />
+        </div>
+      );
+
+    setLoading(true);
+
+    axios
+      .get(
+        `https://api.javascripttutorial.net/v1/quotes/?page=${index}&limit=10`
+      )
+      .then((res) => {
+        setQuotes((prevQoutes) => [...prevQoutes, ...res.data.data]);
+      })
+      .catch((err) => console.log(err));
+    setIndex((prevIndex) => prevIndex + 1);
+
+    setLoading(false);
+  }, [index, loading]);
 
   useEffect(() => {
     const fetchQuotes = async () => {
       try {
         const response = await axios.get(
-          "https://api.javascripttutorial.net/v1/quotes/?page=1&limit=10"
+          `https://api.javascripttutorial.net/v1/quotes/?page=1&limit=10`
         );
-        setQuotes(response.data.data); // Set quotes data array
-        setLoading(false); // Set loading to false after data is fetched
+        setQuotes(response.data.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching quotes:", error);
-        setLoading(false); // Set loading to false if there's an error
+        setLoading(false);
       }
     };
 
     fetchQuotes();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 20) {
+        fetchData();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchData]);
+
   console.log(quotes);
-  // If loading, render loading message
+
   if (loading) {
     return (
-      <div className="h-full bg-blue-950 w-screen justify-items-center text-white">
-        <p>Loading...</p>
+      <div className="h-full flex p-80 justify-center items-center bg-blue-950 text-white">
+        <ReactLoading type="bars" color="white" height={100} width={150} />
       </div>
     );
   }
 
-  // If quotes array is empty, render "No quotes available" message
   if (quotes.length === 0) {
     return (
       <div className="h-full bg-blue-950 w-screen justify-items-center text-white">
@@ -47,9 +85,9 @@ function App() {
 
   return (
     <div className="h-full bg-blue-950 w-screen justify-items-center">
-      <div className="py-12">
+      <div className="py-4">
         {quotes.map((quote) => (
-          <Card key={quote.id} quote={quote} /> // Pass quote data as props
+          <Card key={quote.id} quote={quote} />
         ))}
       </div>
     </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Card = ({ quote }) => {
   return (
-    <div className="mx-96 text-white bg-slate-500 text-center p-2 mb-2">
+    <div className="mx-96 text-white bg-blue-950 text-center p-2 mb-2">
       <div className="flex flex-col">
         <div className="flex flex-row items-start">
-          <div className="mr-2">{quote.id})</div> {/* Add ")" after quote ID */}
+          <div className="mr-2">{quote.id})</div>
           <div className="text-left">{quote.quote}</div>
           <div className="ml-auto text-5xl">''</div>
         </div>
